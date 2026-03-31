9ded39af99b249fb19e263b0a75fc3b7f17af97b
subashveluG <chendurvelu1999@gmail.com>
2026-03-31 19:42:32 +0530

    Standardize branding, footer, and navigation site-wide

--- update_nav.js
+++ update_nav.js
@@ -5,27 +5,10 @@ const dir = 'c:/Users/VISU/OneDrive/Documents/Smartfusion/March_2nd_Slot_SubashC
 const files = fs.readdirSync(dir).filter(f => f.endsWith('.html'));
 
 const newNav = `                      <ul class="navbar-nav justify-content-center flex-grow-1 pe-3 fs-5">
-                          <li class="nav-item"><a class="nav-link fw-semibold" href="index.html">Home</a></li>
-                          <li class="nav-item"><a class="nav-link fw-semibold" href="home2.html">Home 2</a></li>
+                          <li class="nav-item"><a class="nav-link fw-semibold" href="index.html">Home1</a></li>
+                          <li class="nav-item"><a class="nav-link fw-semibold" href="home2.html">Home2</a></li>
                           <li class="nav-item"><a class="nav-link fw-semibold" href="about.html">About</a></li>
                           <li class="nav-item"><a class="nav-link fw-semibold" href="services.html">Services</a></li>
-                          <li class="nav-item dropdown">
-                              <a class="nav-link fw-semibold dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                  Pages
-                              </a>
-                              <ul class="dropdown-menu">
-                                  <li><a class="dropdown-item" href="service-details.html">Service Details</a></li>
-                                  <li><a class="dropdown-item" href="pricing.html">Pricing Plans</a></li>
-                                  <li><a class="dropdown-item" href="blog-details.html">Blog Details</a></li>
-                                  <li><hr class="dropdown-divider"></li>
-                                  <li><a class="dropdown-item" href="dashboard.html">Admin Dashboard</a></li>
-                                  <li><a class="dropdown-item" href="login.html">Login</a></li>
-                                  <li><a class="dropdown-item" href="register.html">Sign Up</a></li>
-                                  <li><hr class="dropdown-divider"></li>
-                                  <li><a class="dropdown-item" href="404.html">404 Error</a></li>
-                                  <li><a class="dropdown-item" href="coming-soon.html">Coming Soon</a></li>
-                              </ul>
-                          </li>
                           <li class="nav-item"><a class="nav-link fw-semibold" href="blog.html">Blog</a></li>
                           <li class="nav-item"><a class="nav-link fw-semibold" href="contact.html">Contact</a></li>
                       </ul>`;
